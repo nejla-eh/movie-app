a37ecca2fa7877f772fbb20d451e115337e5cd60
Nejla <nejlaefendic@hotmail.com>
2022-10-05 15:47:03 +0200

    re-enable authentication for movies endpoint

--- index.js
+++ index.js
@@ -49,16 +49,20 @@ app.get("/", (req, res) => {
 });
 
 // List all movies
-app.get("/movies", (req, res) => {
-  Movies.find()
-    .then((movies) => {
-      res.status(201).json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Get a single movie by title
 app.get(
