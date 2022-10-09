3c826f8c6bbd5dd1262e05472d5f911cecc0eb87
Nejla <nejlaefendic@hotmail.com>
2022-10-09 19:55:46 +0200

    add delete favorite movies endpoint

--- index.js
+++ index.js
@@ -335,6 +335,28 @@ app.post(
 );
 
 app.delete(
+  "/users/:Username/movies/:MovieID",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.findOneAndUpdate(
+      { Username: req.params.Username },
+      {
+        $pull: { FavoriteMovies: req.params.MovieID },
+      },
+      { new: true },
+      (err, updatedUser) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send("Error: " + err);
+        } else {
+          res.json(updatedUser);
+        }
+      }
+    );
+  }
+);
+
+app.delete(
   "/users/:Username",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
